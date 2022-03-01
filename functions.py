dfd08b055ba99bbcd56f7af5db1cf43e13652f92
DerBiologiker <quentink@gmx.de>
2022-03-01 22:24:23 +0100

    SubAdd
    Addition and Subdivision works now
    And you can set a Minus at the Start

--- functions.py
+++ functions.py
@@ -6,8 +6,18 @@ def Calculation(Zahl, InputLine, CalcNumber):
         InputLine.delete(0, tkinter.END)
         CalcNumber.clear()
 
+    elif Zahl == "<-":
+        InputLine.delete(len(InputLine.get()) - 1)
+        if(CalcNumber[-1] != "+" and CalcNumber[-1] != "-" and CalcNumber[-1] != "*" and CalcNumber[-1] != "/"):
+            if(len(str(CalcNumber[-1])) > 1):
+                CalcNumber[-1] = float(str(CalcNumber[-1])[:-1])
+            else:
+                CalcNumber.pop()
+        else:
+            CalcNumber.pop()
+
     elif Zahl != "=":
-        if(Zahl == "+" or Zahl == "*" or Zahl == "/"):
+        if(Zahl == "+" or Zahl == "*" or Zahl == "-" or Zahl == "/"):
             try:
                 if(CalcNumber[-1] != "+" and CalcNumber[-1] != "-" and CalcNumber[-1] != "*" and CalcNumber[-1] != "/"):
                     CalcNumber.append(Zahl)
@@ -19,14 +29,14 @@ def Calculation(Zahl, InputLine, CalcNumber):
                         InputLine.insert(tkinter.END, i)
             except IndexError:
                 if(Zahl == "-"):
-                    FirstNumberNegative = True
                     InputLine.insert(tkinter.END, Zahl)
                     CalcNumber.append(Zahl)
                 print("No number entered yet!")
-                # Außer es wurde Minus gedrückt wegen Vorzeichen
         else:
             if(CalcNumber):
                 if(CalcNumber[-1] != "+" and CalcNumber[-1] != "-" and CalcNumber[-1] != "*" and CalcNumber[-1] != "/"):
+                    CalcNumber[-1] = CalcNumber[-1] + str(Zahl)
+                elif(CalcNumber[-1] == "-" and len(CalcNumber) == 1):
                     CalcNumber[-1] = CalcNumber[-1] + str(Zahl)
                 else:
                     CalcNumber.append(str(Zahl))
@@ -56,34 +66,65 @@ def Calculation(Zahl, InputLine, CalcNumber):
                     DivideExists = True
 
                 if(DivideExists == True or (MultiplyExists == False and PostionMultiply < PostionDivide)):
-                    resultCalc = int(
-                        CalcNumber[PostionMultiply - 1]) * int(CalcNumber[PostionMultiply + 1])
-
-                    print("resultCalc:", resultCalc)
-                    print(PostionMultiply)
+                    resultCalc = float(
+                        CalcNumber[PostionMultiply - 1]) * float(CalcNumber[PostionMultiply + 1])
 
                     CalcNumber.pop(PostionMultiply - 1)
                     CalcNumber.pop(PostionMultiply - 1)
                     CalcNumber.pop(PostionMultiply - 1)
                     CalcNumber.insert(PostionMultiply - 1, resultCalc)
 
-                    print(CalcNumber)
                     InputLine.delete(0, tkinter.END)
                     for i in CalcNumber:
                         InputLine.insert(tkinter.END, i)
                 else:
-                    resultCalc = int(
-                        CalcNumber[PostionDivide - 1]) / int(CalcNumber[PostionDivide + 1])
-
-                    print("resultCalc:", resultCalc)
-                    print(PostionDivide)
+                    resultCalc = float(
+                        CalcNumber[PostionDivide - 1]) / float(CalcNumber[PostionDivide + 1])
 
                     CalcNumber.pop(PostionDivide - 1)
                     CalcNumber.pop(PostionDivide - 1)
                     CalcNumber.pop(PostionDivide - 1)
                     CalcNumber.insert(PostionDivide - 1, resultCalc)
 
-                    print(CalcNumber)
+                    InputLine.delete(0, tkinter.END)
+                    for i in CalcNumber:
+                        InputLine.insert(tkinter.END, i)
+
+            while (CalcNumber.count("+") != 0 or CalcNumber.count("-") != 0):
+                MultiplyExists = False
+                DivideExists = False
+                Positions = []
+                try:
+                    PostionMultiply = CalcNumber.index("+")
+                except:
+                    MultiplyExists = True
+
+                try:
+                    PostionDivide = CalcNumber.index("-")
+                except:
+                    DivideExists = True
+
+                if(DivideExists == True or (MultiplyExists == False and PostionMultiply < PostionDivide)):
+                    resultCalc = float(
+                        CalcNumber[PostionMultiply - 1]) + float(CalcNumber[PostionMultiply + 1])
+
+                    CalcNumber.pop(PostionMultiply - 1)
+                    CalcNumber.pop(PostionMultiply - 1)
+                    CalcNumber.pop(PostionMultiply - 1)
+                    CalcNumber.insert(PostionMultiply - 1, resultCalc)
+
+                    InputLine.delete(0, tkinter.END)
+                    for i in CalcNumber:
+                        InputLine.insert(tkinter.END, i)
+                else:
+                    resultCalc = float(
+                        CalcNumber[PostionDivide - 1]) - float(CalcNumber[PostionDivide + 1])
+
+                    CalcNumber.pop(PostionDivide - 1)
+                    CalcNumber.pop(PostionDivide - 1)
+                    CalcNumber.pop(PostionDivide - 1)
+                    CalcNumber.insert(PostionDivide - 1, resultCalc)
+
                     InputLine.delete(0, tkinter.END)
                     for i in CalcNumber:
                         InputLine.insert(tkinter.END, i)
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ CalcNumber = []
 Fenster = tkinter.Tk()
 
 Fenster.title("Calculator")
-Fenster.geometry("300x400")
+Fenster.geometry("375x400")
 
 
 InputLine = tkinter.Entry(
@@ -18,109 +18,116 @@ Button1 = tkinter.Button(
     text="1",
     command=lambda: Calculation(1, InputLine, CalcNumber)
 )
-Button1.place(relx=0.25, rely=0.09, relwidth=0.25, relheight=0.18, anchor='nw')
+Button1.place(relx=0.2, rely=0.09, relwidth=0.2, relheight=0.18, anchor='nw')
 
 Button2 = tkinter.Button(
     text="2",
     command=lambda: Calculation(2, InputLine, CalcNumber)
 )
-Button2.place(relx=0.5, rely=0.09, relwidth=0.25, relheight=0.18, anchor='nw')
+Button2.place(relx=0.4, rely=0.09, relwidth=0.2, relheight=0.18, anchor='nw')
 
 Button3 = tkinter.Button(
     text="3",
     command=lambda: Calculation(3, InputLine, CalcNumber)
 )
-Button3.place(relx=0.75, rely=0.09, relwidth=0.25, relheight=0.18, anchor='nw')
+Button3.place(relx=0.6, rely=0.09, relwidth=0.2, relheight=0.18, anchor='nw')
 
 Button4 = tkinter.Button(
     text="4",
     command=lambda: Calculation(4, InputLine, CalcNumber)
 )
-Button4.place(relx=0.25, rely=0.27, relwidth=0.25,
+Button4.place(relx=0.2, rely=0.27, relwidth=0.2,
               relheight=0.18, anchor='nw')
 
 Button5 = tkinter.Button(
     text="5",
     command=lambda: Calculation(5, InputLine, CalcNumber)
 )
-Button5.place(relx=0.5, rely=0.27, relwidth=0.25,
+Button5.place(relx=0.4, rely=0.27, relwidth=0.2,
               relheight=0.18, anchor='nw')
 
 Button6 = tkinter.Button(
     text="6",
     command=lambda: Calculation(6, InputLine, CalcNumber)
 )
-Button6.place(relx=0.75, rely=0.27, relwidth=0.25,
+Button6.place(relx=0.6, rely=0.27, relwidth=0.2,
               relheight=0.18, anchor='nw')
 
 Button7 = tkinter.Button(
     text="7",
     command=lambda: Calculation(7, InputLine, CalcNumber)
 )
-Button7.place(relx=0.25, rely=0.45, relwidth=0.25,
+Button7.place(relx=0.2, rely=0.45, relwidth=0.2,
               relheight=0.18, anchor='nw')
 
 Button8 = tkinter.Button(
     text="8",
     command=lambda: Calculation(8, InputLine, CalcNumber)
 )
-Button8.place(relx=0.5, rely=0.45, relwidth=0.25, relheight=0.18, anchor='nw')
+Button8.place(relx=0.4, rely=0.45, relwidth=0.2, relheight=0.18, anchor='nw')
 
 Button9 = tkinter.Button(
     text="9",
     command=lambda: Calculation(9, InputLine, CalcNumber)
 )
-Button9.place(relx=0.75, rely=0.45, relwidth=0.25,
+Button9.place(relx=0.6, rely=0.45, relwidth=0.2,
               relheight=0.18, anchor='nw')
 
 Button0 = tkinter.Button(
     text="0",
     command=lambda: Calculation(0, InputLine, CalcNumber)
 )
-Button0.place(relx=0.25, rely=0.63, relwidth=0.25,
+Button0.place(relx=0.2, rely=0.63, relwidth=0.2,
               relheight=0.18, anchor='nw')
 
 ButtonEqual = tkinter.Button(
     text="=",
     command=lambda: Calculation("=", InputLine, CalcNumber)
 )
-ButtonEqual.place(relx=0.5, rely=0.63, relwidth=0.5,
+ButtonEqual.place(relx=0.4, rely=0.63, relwidth=0.4,
                   relheight=0.18, anchor='nw')
 
 ButtonPlus = tkinter.Button(
     text="+",
     command=lambda: Calculation("+", InputLine, CalcNumber)
 )
-ButtonPlus.place(relx=0, rely=0.09, relwidth=0.25,
+ButtonPlus.place(relx=0, rely=0.09, relwidth=0.2,
                  relheight=0.18, anchor='nw')
 
 ButtonMinus = tkinter.Button(
     text="-",
     command=lambda: Calculation("-", InputLine, CalcNumber)
 )
-ButtonMinus.place(relx=0, rely=0.27, relwidth=0.25,
+ButtonMinus.place(relx=0, rely=0.27, relwidth=0.2,
                   relheight=0.18, anchor='nw')
 
 ButtonMultiply = tkinter.Button(
     text="*",
     command=lambda: Calculation("*", InputLine, CalcNumber)
 )
-ButtonMultiply.place(relx=0, rely=0.45, relwidth=0.25,
+ButtonMultiply.place(relx=0, rely=0.45, relwidth=0.2,
                      relheight=0.18, anchor='nw')
 
 ButtonDivide = tkinter.Button(
     text="/",
     command=lambda: Calculation("/", InputLine, CalcNumber)
 )
-ButtonDivide.place(relx=0, rely=0.63, relwidth=0.25,
+ButtonDivide.place(relx=0, rely=0.63, relwidth=0.2,
                    relheight=0.18, anchor='nw')
 
 ButtonClear = tkinter.Button(
     text="C",
     command=lambda: Calculation("C", InputLine, CalcNumber)
 )
-ButtonClear.place(relx=0, rely=0.81, relwidth=0.5,
+ButtonClear.place(relx=0, rely=0.81, relwidth=0.4,
                   relheight=0.18, anchor='nw')
+
+ButtonStepBack = tkinter.Button(
+    text="<-",
+    command=lambda: Calculation("<-", InputLine, CalcNumber)
+)
+ButtonStepBack.place(relx=0.4, rely=0.81, relwidth=0.2,
+                     relheight=0.18, anchor='nw')
 
 
 Fenster.mainloop()
